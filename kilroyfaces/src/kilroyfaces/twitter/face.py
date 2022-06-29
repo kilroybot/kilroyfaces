90ee730d828cdbc3be7a32deb42f7dca21bd7a35
Sebastian Pietras <01133337@pw.edu.pl>
2022-06-29 22:57:40 +0200

    Upgraded `kilroyshare` version (#16)

--- kilroyfaces/src/kilroyfaces/twitter/face.py
+++ kilroyfaces/src/kilroyfaces/twitter/face.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Optional, Tuple
+from typing import AsyncIterator, Iterator, Optional, Tuple, Union
 
 import tweepy
 from kilroyshare import Face
@@ -10,6 +10,8 @@ from kilroyfaces.twitter.utils import tweet
 
 
 class TwitterFace(Face[str, str]):
+    client: tweepy.Client = None
+
     def __init__(
         self,
         auth: Auth,
@@ -17,24 +19,25 @@ class TwitterFace(Face[str, str]):
         scraper: TweetScraper = FollowingScraper(),
     ) -> None:
         super().__init__()
+        self.auth = auth
         self.scorer = scorer
         self.scraper = scraper
-        self.client = self.build_client(auth)
 
-    @staticmethod
-    def build_client(auth: Auth) -> tweepy.Client:
-        return tweepy.Client(
-            consumer_key=auth.consumer_key,
-            consumer_secret=auth.consumer_secret,
-            access_token=auth.token,
-            access_token_secret=auth.secret,
+    async def init(self) -> None:
+        self.client = tweepy.Client(
+            consumer_key=self.auth.consumer_key,
+            consumer_secret=self.auth.consumer_secret,
+            access_token=self.auth.token,
+            access_token_secret=self.auth.secret,
         )
 
-    def scrap(self, limit: Optional[int] = None) -> Iterator[Tuple[str, str]]:
+    async def scrap(
+        self, limit: Optional[int] = None
+    ) -> Union[Iterator[Tuple[str, str]], AsyncIterator[Tuple[str, str]]]:
         return self.scraper.scrap(limit, self.client)
 
-    def post(self, data: str) -> str:
+    async def post(self, data: str) -> str:
         return tweet(data, self.client)
 
-    def score(self, post_id: str) -> float:
+    async def score(self, post_id: str) -> float:
         return self.scorer.score(post_id, self.client)
